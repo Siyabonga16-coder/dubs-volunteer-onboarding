846e999f8ec3d972cd7eab5adc324ae1a85710af
Nhlakanipho <mnguninhlakanipho482@gmail.com>
2026-03-12 10:10:16 +0200

    Add files via upload

---
+++ scripts/extract_to_csv.py
@@ -0,0 +1,33 @@
+from docx import Document
+import csv
+
+
+doc = Document("path of the actual document ")
+
+for para in doc.paragraphs:#now printing all the texts contained in the file.
+    print(para.text)
+#this step act as  a discovery step to see format
+volunteer_data = {
+    "Name": "",
+    "Surname": "",
+    "DateOfBirth": "",
+    "Cellphone": "",
+    "Email": "",
+    
+    # waiting for the form format to come the form created
+    
+}
+for para in doc.paragraphs:
+    text = para.text.strip()
+    if text.startswith("Name:"):
+        volunteer_data["Name"]= text.replace("Name:","").strip()
+    elif text.startswith("Surname:"):
+        volunteer_data["Surname"]=text.replace("Surname:","").strip()
+    elif text.startswith("Date of Birth:"):
+        volunteer_data["DateOfBirth"] = text.replace("Date of Birth:", "").strip()
+    elif text.startswith("Cellphone:"):
+        volunteer_data["Cellphone"] = text.replace("Cellphone:", "").strip()
+    elif text.startswith("Email:"):
+        volunteer_data["Email"] = text.replace("Email:", "").strip()
+        
+    # wiating for another 
